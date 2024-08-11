1ddb2f6173b03740dc96ea56bd5357a3ca7dd1c5
chicken555556 <nanthanshetty26@gmail.com>
2024-08-11 11:15:20 +0530

    Update NQueenProblem.c

--- NQueenProblem.c
+++ NQueenProblem.c
@@ -1,62 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-int x[20];
-// k->row i->column
-int place(int k, int i)
+#include<stdio.h>
+#include<stdlib.h>
+
+int place(int p,int x[])
 {
-    int j;
-    for (j = 1; j <= k - 1; j++)
-    {
-        if ((x[j] == i) || (abs(x[j] - i) == abs(j - k)))
-            return 0;
-    }
-    return 1;
+        int i;
+        for(i=1;i<=(p-1);i++)
+                if((x[i]==x[p])||(abs(x[i]-x[p])==(abs(i-p))))
+                                return 0;
+        return 1;
 }
-void printQueen(int n)
-{
-    int i, j;
-    static int count = 1;
-    printf("\n\nSolution %d is- \n\n", count++);
-    for (i = 1; i <= n; ++i)
-        printf("\t%d", i);
-    for (i = 1; i <= n; ++i)
-    {
-        printf("\n\n%d", i);
-        for (j = 1; j <= n; ++j) // for nxn board
-        {
-            if (x[i] == j)
-                printf("\tQ"); // queen at i,j position
-            else
-                printf("\t-"); // empty slot
-        }
-    }
-}
-void NQueen(int k, int n)
-{
-    int i;
-    for (i = 1; i <= n; i++)
-    {
-        if (place(k, i))
-        {
-            x[k] = i;
-            if (k == n)
-            {
-                printQueen(n);
-            }
-            else
-                NQueen(k + 1, n);
-        }
-    }
-}
+
 int main()
 {
-    int n;
-    printf("\nN-QUEEN PROBLEM\n");
-    printf("\nEnter the number of queens to be placed - ");
-    scanf("%d", &n);
-    if (n == 2 || n == 3 || n == 0)
-        printf("\nNo solutions possible!!");
-    else
-        NQueen(1, n);
-    return (0);
+       int i,j,k,n,count=1,x[1500];
+       printf("enter the number of queens");
+       scanf("%d",&n);
+       if(n==0||n==2||n==3)
+       printf("no solution!!!");
+       else
+       {
+           k=1;
+           x[1]=0;
+           while(k)
+           {
+                 x[k]=x[k]+1;
+                 while(x[k]<=n&&!place(k,x))
+                 x[k]=x[k]+1;
+                 if(x[k]<=n)
+                 {
+                       if(k==n)
+                       {
+                               printf("solution %d\n",count++);
+                               for(i=1;i<=n;i++)
+                               {
+                                       for(j=1;j<x[i];j++)
+                                       printf("* ");
+                                       printf("Q ");
+                                       for(j=x[i]+1;j<=n;j++)
+                                       printf("* " );
+                                       printf("\n");
+                               }
+                        }
+                        else
+                        {
+                               k+=1;
+                               x[k]=0;
+                        }
+                 }
+                 else
+                      k-=1;
+         }
+
+     }
 }
